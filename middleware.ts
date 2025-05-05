53d7092b49f85a4a90511781b086a2f0885dbb23
Eddi3MS <edsonmarc2014@gmail.com>
2025-05-05 17:56:25 -0300

    update

--- middleware.ts
+++ middleware.ts
@@ -64,6 +64,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
+  runtime: 'nodejs',
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
