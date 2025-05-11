799ed4d4b826906cb38c8b556f720d9cbba64f49
Eddi3MS <edsonmarc2014@gmail.com>
2025-05-11 12:09:46 -0300

    up manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'App de agendamentos flexível',
     start_url: '/',
     display: 'standalone',
-    background_color: '#000000',
-    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    theme_color: '#000000',
     icons: [
       {
         src: '/logo-192x192.png',
