839cf1dfeae1900bda91c19113a9460def7ad94d
Eddi3MS <edsonmarc2014@gmail.com>
2025-05-11 14:19:32 -0300

    fix scheduler list

--- app/(authenticated)/user/components/scheduler-list.tsx
+++ app/(authenticated)/user/components/scheduler-list.tsx
@@ -22,15 +22,39 @@ import { formatBRL } from '@/lib/intl'
 import { cn } from '@/lib/utils'
 import { Appointment } from '@/types/appointment'
 import { isToday, parseISO } from 'date-fns'
-import { CheckIcon, ListFilterIcon } from 'lucide-react'
+import { CheckIcon, ListFilterIcon, Loader2 } from 'lucide-react'
 import { useState } from 'react'
 import AppointmentCancelButton from './appointment-cancel-button'
+import { useQuery } from '@tanstack/react-query'
 
-export default function SchedulerList({
-  appointmentsInit,
-}: {
-  appointmentsInit: Appointment[]
-}) {
+export default function SchedulerList() {
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['user-appointments'],
+    queryFn: async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_PATH}/api/appointments/me`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+          }
+        )
+
+        const data = await res.json()
+
+        if (!res.ok) {
+          throw new Error(data?.message || 'Erro ao buscar agendamentos.')
+        }
+
+        return data as Appointment[]
+      } catch (error) {
+        throw new Error('Erro de conexão com o servidor.')
+      }
+    },
+  })
   const [showCanceled, setShowCanceled] = useState(true)
 
   const handleShow = () => {
@@ -38,8 +62,8 @@ export default function SchedulerList({
   }
 
   const appointmentsFiltered = showCanceled
-    ? appointmentsInit
-    : appointmentsInit.filter((ap) => !ap.canceled)
+    ? data
+    : data?.filter((ap) => !ap.canceled)
   return (
     <>
       <div className="flex justify-between items-center">
@@ -62,64 +86,84 @@ export default function SchedulerList({
       </div>
 
       <ScrollArea className="h-[calc(100dvh-200px)] w-full rounded-md border p-4">
-        <MotionCardsWrapper>
-          {appointmentsFiltered.map((appointment) => {
-            const isBefore =
-              isBeforeToday(parseISO(appointment.date)) ||
-              (isToday(parseISO(appointment.date)) &&
-                isTimeBeforeNow(appointment.time))
-            const isAfterToday =
-              !isBeforeToday(parseISO(appointment.date)) &&
-              !isToday(parseISO(appointment.date))
-            return (
-              <Card
-                key={appointment._id}
-                className={cn(
-                  `h-full transition-all hover:shadow-lg`,
-                  isBefore ? 'border-red-500 opacity-60' : ''
-                )}
-              >
-                <CardContent className="p-4 grid gap-2 w-full relative">
-                  {appointment.canceled ? (
-                    <Badge
-                      variant="destructive"
-                      className="absolute bottom-2 right-2"
-                    >
-                      Cancelado
-                    </Badge>
-                  ) : (isToday(parseISO(appointment.date)) &&
-                      isTimeTwoHoursAfterNow(appointment.time)) ||
-                    isAfterToday ? (
-                    <AppointmentCancelButton id={appointment._id} />
-                  ) : null}
-                  <div className="flex justify-between">
-                    <Badge variant="outline">
-                      {formatDateToDDMMYYYY(appointment.date)}
-                    </Badge>
-                    <span className="text-gray-500">
-                      horário: {appointment.time}
-                    </span>
-                  </div>
-                  <div className="flex flex-col gap-2 justify-between">
-                    <p className="font-semibold">
-                      Profissional: {appointment.providerId.userId.name}
-                    </p>
+        {isLoading ? (
+          <div className="flex flex-1 items-center justify-center w-full h-[calc(100dvh-400px)]">
+            <Loader2 className="animate-spin" />
+          </div>
+        ) : !isLoading && error ? (
+          <div className="grid place-items-center gap-4 p-8">
+            <p>Algo deu errado na listagem.</p>
+            {error?.message ? <p>Erro: {error?.message}</p> : null}
+          </div>
+        ) : !isLoading &&
+          !error &&
+          (!appointmentsFiltered || !appointmentsFiltered?.length) ? (
+          <div className="grid place-items-center gap-4 p-8">
+            <p className="text-center">Nenhum agendamento encontrado.</p>
+          </div>
+        ) : (
+          <MotionCardsWrapper>
+            {appointmentsFiltered?.map((appointment) => {
+              const isBefore =
+                isBeforeToday(parseISO(appointment.date)) ||
+                (isToday(parseISO(appointment.date)) &&
+                  isTimeBeforeNow(appointment.time))
+              const isAfterToday =
+                !isBeforeToday(parseISO(appointment.date)) &&
+                !isToday(parseISO(appointment.date))
+              return (
+                <Card
+                  key={appointment._id}
+                  className={cn(
+                    `h-full transition-all hover:shadow-lg`,
+                    isBefore ? 'border-red-500 opacity-60' : ''
+                  )}
+                >
+                  <CardContent className="p-4 grid gap-2 w-full relative">
+                    {appointment.canceled ? (
+                      <Badge
+                        variant="destructive"
+                        className="absolute bottom-2 right-2"
+                      >
+                        Cancelado
+                      </Badge>
+                    ) : (isToday(parseISO(appointment.date)) &&
+                        isTimeTwoHoursAfterNow(appointment.time)) ||
+                      isAfterToday ? (
+                      <AppointmentCancelButton
+                        id={appointment._id}
+                        refetch={() => refetch()}
+                      />
+                    ) : null}
+                    <div className="flex justify-between">
+                      <Badge variant="outline">
+                        {formatDateToDDMMYYYY(appointment.date)}
+                      </Badge>
+                      <span className="text-gray-500">
+                        horário: {appointment.time}
+                      </span>
+                    </div>
+                    <div className="flex flex-col gap-2 justify-between">
+                      <p className="font-semibold">
+                        Profissional: {appointment.providerId.userId.name}
+                      </p>
 
-                    <p className="font-semibold">
-                      Serviço: {appointment.serviceId.name}
-                    </p>
-                    <p className="font-semibold">
-                      Preço: {formatBRL(appointment.serviceId.price)}
-                    </p>
-                    <p className="font-semibold">
-                      Duração: {appointment.serviceId.duration} min.
-                    </p>
-                  </div>
-                </CardContent>
-              </Card>
-            )
-          })}
-        </MotionCardsWrapper>
+                      <p className="font-semibold">
+                        Serviço: {appointment.serviceId.name}
+                      </p>
+                      <p className="font-semibold">
+                        Preço: {formatBRL(appointment.serviceId.price)}
+                      </p>
+                      <p className="font-semibold">
+                        Duração: {appointment.serviceId.duration} min.
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              )
+            })}
+          </MotionCardsWrapper>
+        )}
       </ScrollArea>
     </>
   )
--- app/(authenticated)/user/page.tsx
+++ app/(authenticated)/user/page.tsx
@@ -1,28 +1,5 @@
-import { Button } from '@/components/ui/button'
-import { getOwnAppointments } from '@/http/fetch-appointment'
-import Link from 'next/link'
 import SchedulerList from './components/scheduler-list'
-import { redirect } from 'next/navigation'
 
 export default async function UserPage() {
-  const res = await getOwnAppointments()
-
-  if (!res.success) {
-    redirect(`/feedback?error=${res.error}`)
-  }
-
-  if (!res.data.length) {
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold">Nenhum dado para exibir!</h1>
-        <p className="text-lg ">Deseja fazer um agendamento?</p>
-        <Button asChild variant="link">
-          <Link href="/scheduler" className="text-orange-500 underline">
-            Agendar
-          </Link>
-        </Button>
-      </div>
-    )
-  }
-  return <SchedulerList appointmentsInit={res.data} />
+  return <SchedulerList />
 }
