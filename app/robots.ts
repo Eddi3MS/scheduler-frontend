016c04ceb0e30bb24b9261eaf502aa7279132388
Eddi3MS <edsonmarc2014@gmail.com>
2025-05-11 12:06:40 -0300

    update robots && sw.js

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
+        allow: ['/register', '/'],
         disallow: ['/admin/', '/provider/', '/user/'],
       },
     ],
