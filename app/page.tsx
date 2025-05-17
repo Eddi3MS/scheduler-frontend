bc45b6d283493d236f336568a86d798637c6d6bf
Eddi3MS <edsonmarc2014@gmail.com>
2025-05-17 10:54:31 -0300

    fix image

--- app/page.tsx
+++ app/page.tsx
@@ -128,8 +128,7 @@ export default function Home() {
               <div className="relative aspect-[16/9] overflow-hidden rounded-2xl bg-white/10  backdrop-blur-sm shadow-md">
                 <Image
                   src="/barber.jpg"
-                  width={768}
-                  height={512}
+                  layout="fill"
                   alt="barber shop"
                   className="w-full aspect-[16/9]"
                 />
